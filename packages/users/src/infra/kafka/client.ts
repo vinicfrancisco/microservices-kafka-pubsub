b2d3d361f352c090758909c5d7ffc4cfbed6b07c
vinicfrancisco <vini.ibi@gmail.com>
2020-08-11 11:16:23 -0300

    removed dot env variable

--- packages/users/src/infra/kafka/client.ts
+++ packages/users/src/infra/kafka/client.ts
@@ -1,7 +1,7 @@
 import { Kafka, logLevel } from 'kafkajs';
 
 const kafka = new Kafka({
-  brokers: [`${process.env.IP_ADDRESS}:9092`],
+  brokers: ['192.168.0.231:9092'],
   logLevel: logLevel.WARN,
   clientId: 'users',
 });
