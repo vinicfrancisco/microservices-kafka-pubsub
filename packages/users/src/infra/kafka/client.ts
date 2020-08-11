56c79c3821254998f543e581563a713745247ba5
vinicfrancisco <vini.ibi@gmail.com>
2020-08-11 11:14:25 -0300

    added IP address to env variable

--- packages/users/src/infra/kafka/client.ts
+++ packages/users/src/infra/kafka/client.ts
@@ -1,7 +1,7 @@
 import { Kafka, logLevel } from 'kafkajs';
 
 const kafka = new Kafka({
-  brokers: ['192.168.2.103:9092'],
+  brokers: [`${process.env.IP_ADDRESS}:9092`],
   logLevel: logLevel.WARN,
   clientId: 'users',
 });
